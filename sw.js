0f0e3402baef851fa9080b0e795a30012f298624
trantechsolutions <104873115+trantechsolutions@users.noreply.github.com>
2025-04-08 13:51:05 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = "lacrosse-scoreboard-v1";
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/vue@3'
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
